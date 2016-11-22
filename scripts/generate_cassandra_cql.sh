ba215c5351bf438dced1be5d033ffead680b08b0
Brian McClain <brianmmcclain@gmail.com>
2016-11-22 09:40:02 -0500

    nval 1

--- scripts/generate_cassandra_cql.sh
+++ scripts/generate_cassandra_cql.sh
@@ -2,7 +2,7 @@
 
 SIZE="${1:-10}"
 COUNT=0
-CMD="DROP KEYSPACE ycsb; CREATE KEYSPACE ycsb WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}; CREATE TABLE ycsb.usertable (y_id varchar primary key"
+CMD="DROP KEYSPACE ycsb; CREATE KEYSPACE ycsb WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}; CREATE TABLE ycsb.usertable (y_id varchar primary key"
 
 while [ $COUNT -lt $SIZE ]; do
 	CMD="$CMD, field$COUNT text"
